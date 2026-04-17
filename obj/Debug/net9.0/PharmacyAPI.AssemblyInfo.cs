e50cc05d3ed5b51f0b1da2f952ea39b3c6c15592
Munirathinam_E <munirathinam6724@gmail.com>
2026-04-17 13:18:21 +0530

    Modules Added

--- obj/Debug/net9.0/PharmacyAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/PharmacyAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7c0bef07d012cfec07f994175c6e076a148359")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmacyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
